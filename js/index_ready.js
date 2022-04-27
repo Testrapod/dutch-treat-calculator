d914ddcca59304fb3de4f46b5a6324ccc5c5aa04
10EastSea <xgf6580@gmail.com>
2022-04-27 21:38:23 +0900

    member add & revise

--- js/index_ready.js
+++ js/index_ready.js
@@ -1,4 +1,8 @@
 /* index.html document ready */
 $(document).ready(function () {
-    
+    // add member
+    $("#add_button_member").click(function() {
+        var name = $("#add_input_member").val();
+        addMember(name);
+    });
 });
---
+++ js/member.js
@@ -0,0 +1,38 @@
+var member_count = 3;
+
+function addMember(name) {
+    member_count++;
+
+    var tagContent =
+        '<tr>' +
+            '<td>' + member_count + '</td>' +
+            '<td onclick="reviseMember(this);">' + name + '</td>' +
+            '<td class="delete-button" onClick="deleteMember(this);">X</td>' +
+        '</tr>';
+    
+    $("#table_member>tbody").append(tagContent);
+    $("#add_input_member").val('');
+}
+
+function reviseMember(obj) {
+    var name = $(obj).text().trim();
+    $("#revise_input_member").val(name);
+
+    var reviseModalMember = new bootstrap.Modal(document.getElementById('reviseModalMember'));
+    reviseModalMember.show();
+
+    $("#revise_button_member").click(function() {
+        name = $("#revise_input_member").val();
+        $(obj).text(name);
+        $("#revise_button_member").off();
+    });
+}
+
+function deleteMember(obj) {
+    var tds = $(obj).parent().children();
+
+    var number = tds.eq(0).text().trim();
+    var name = tds.eq(1).text().trim();
+
+    console.log(number + " / " + name);
+}
