d5862ecbadb9f13324bdcc3ee3a808337a64c4a6
10EastSea <xgf6580@gmail.com>
2022-04-27 23:16:18 +0900

    receipt & result template

--- js/index_ready.js
+++ js/index_ready.js
@@ -1,5 +1,6 @@
 /* index.html document ready */
 $(document).ready(function () {
+    ////////////////////////////////////////////////////////////////////////////////
     // add member
     $("#add_button_member").click(function() {
         var name = $("#add_input_member").val();
@@ -8,4 +9,44 @@ $(document).ready(function () {
         $("#alert_add_success").show();
         setTimeout(function() { $("#alert_add_success").fadeOut(); }, 2000);
     });
+
+    // next (member)
+    $("#next_button_member").click(function() {
+        $("#member").hide();
+        $("#receipt").show();
+    });
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // add receipt
+    $("#add_button_receipt").click(function() {
+        console.log("add receipt");
+
+        // $("#alert_add_success").show();
+        // setTimeout(function() { $("#alert_add_success").fadeOut(); }, 2000);
+    });
+
+    // prev (receipt)
+    $("#prev_button_receipt").click(function() {
+        $("#receipt").hide();
+        $("#member").show();
+    });
+    // next (receipt)
+    $("#next_button_receipt").click(function() {
+        $("#receipt").hide();
+        $("#result").show();
+    });
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // prev (result)
+    $("#prev_button_result").click(function() {
+        $("#result").hide();
+        $("#receipt").show();
+    });
+    // again (result)
+    $("#again_button_result").click(function() { location.reload(); });
+    ////////////////////////////////////////////////////////////////////////////////
 });
